9bf15f0ba8a5c7348b1f326762a165f0a0a1c867
Gaia-LA <gaiaperez@live.com.au>
2025-10-31 18:53:50 +1100

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Whiteboard from "./Whiteboard";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <Whiteboard />
+    </div>
+  );
+}
