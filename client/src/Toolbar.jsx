3a74ba7e898269a971c794ba7744928b64dc1eb1
Gaia-LA <gaiaperez@live.com.au>
2025-10-31 19:04:23 +1100

    Update Toolbar.jsx

--- client/src/Toolbar.jsx
+++ client/src/Toolbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Toolbar({ color, setColor, size, setSize, clearBoard }) {
+export default function Toolbar({ color, setColor, size, setSize, clearBoard, undo }) {
   return (
     <div className="toolbar">
       <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
       <input type="range" min="1" max="20" value={size} onChange={(e) => setSize(e.target.value)} />
+      <button onClick={undo}>Undo</button>
       <button onClick={clearBoard}>Clear Board</button>
     </div>
   );
